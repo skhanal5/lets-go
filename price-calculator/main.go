19ce93277c117cc606eade91c9b921c869ade456
Subodh Khanal <skhanal5@terpmail.umd.edu>
2024-06-22 16:46:57 -0400

    Fix compilation error in price-calculator/main

--- price-calculator/main.go
+++ price-calculator/main.go
@@ -9,16 +9,16 @@ import (
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 	doneChans := make([]chan bool, len(taxRates))
-	errorChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan error, len(taxRates))
 
 	for index, taxRate := range taxRates {
 
 		doneChans[index] = make(chan bool)
-		errorChans[index] = make(chan errors)
+		errorChans[index] = make(chan error)
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		// cmdm := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		go priceJob.Process(doneChans[index])
+		go priceJob.Process(doneChans[index], errorChans[index])
 
 		// if err != nil {
 		// 	fmt.Println("Could not process job")
